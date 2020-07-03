5795e189803946d689288fb465d0bcfb22f41fb4
Alo-gi <65797988+Alo-gi@users.noreply.github.com>
2020-07-02 23:36:20 -0500

    Update script.js

--- module5_solution/js/script.js
+++ module5_solution/js/script.js
@@ -120,14 +120,16 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
-        $dc.loadMenuItems();
+      var homeHtmlToInsertIntoMainPage = $dc.loadMenuItems(chosenCategoryShortName);
       // var homeHtmlToInsertIntoMainPage = ....
-          var insertProperty = function ( string, propName, propValue) {
+        
+          var insertProperty = function (string, propName, propValue) {
             var propToReplace  = "{{" + randomCategoryShortName + "}}";
             string = string
             .replace(new RegExp(propToReplace, "g"), propValue);
               return string;
           };
+          
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
